fda620c072ac1ded88282bcaf66911a7bf31aeb5
Nalluru Sunil Reddy <Nalluru Sunil Reddy@DESKTOP-B8J6KD7>
2018-10-14 15:23:38 +0530

    First commit: created the project, configured Spring MVC and Spring Security through JDBC Auth

---
+++ sanal_srp/src/main/java/org/sanal/config/core/SpringMvcInitializer.java
@@ -0,0 +1,23 @@
+package org.sanal.config.core;
+
+import org.sanal.config.AppConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+	
+}
---
+++ sanal_srp/src/main/java/org/sanal/test/Test.java
@@ -0,0 +1,24 @@
+package org.sanal.test;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author sanal567 Date 14-10-18 00:22 AM
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("kjhgfdsa"));
+	}
+
+}
